1caac73b6f31b8a16f5cdd0e77c70db9c3f2bd8b
Pedro Matheus <pedro.matheusfs@gmail.com>
2025-04-06 19:45:16 -0300

    mudancas para server funfar

--- marcacaoconsultasexames/backend/index.js
+++ marcacaoconsultasexames/backend/index.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 
 app.use("/usuarios", usersRoutes);
 app.get("/", (req, res) => res.send("Bem vindo a API de Usuários"));
-app.all("*", (req, res) =>res.send("Essa página não existe."));
+// app.all("/(.*)/", (req, res) =>res.send("Essa página não existe."));
 
 app.listen(PORT, () =>console.log(`Servidor executando em: http://localhost:${PORT}`));
